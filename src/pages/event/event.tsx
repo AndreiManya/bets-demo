2dc910199a7011f50aae7a6d51d0d9767acea2db
AndreiManya <60035201+AndreiManya@users.noreply.github.com>
2023-07-04 11:48:53 +0300

    feat: style selected event page

--- src/pages/event/event.tsx
+++ src/pages/event/event.tsx
@@ -16,7 +16,7 @@ const Event: FC = () => {
 
   return (
     <div className="selected">
-      <button className="selected__back" onClick={() => navigate(MAIN_ROUTE)}>
+      <button className="button selected__back" onClick={() => navigate(MAIN_ROUTE)} type="button">
         Back
       </button>
       <p className="selected__text">
@@ -25,6 +25,28 @@ const Event: FC = () => {
       <p className="selected__title">
         {itemData?.comandA} - {itemData?.comandB}
       </p>
+      <div className="selected__bet-wrapper">
+        <div className={`bet-item ${select === 1 && 'active'}`} onClick={() => setSelect(1)}>
+          <span className="bet-item__value">{itemData?.win}</span>
+          <span className="bet-item__text">Owners wins</span>
+        </div>
+        <div className={`bet-item ${select === 2 && 'active'}`} onClick={() => setSelect(2)}>
+          <span className="bet-item__value">{itemData?.draw}</span>
+          <span className="bet-item__text">Draw</span>
+        </div>
+        <div className={`bet-item ${select === 3 && 'active'}`} onClick={() => setSelect(3)}>
+          <span className="bet-item__value">{itemData?.lose}</span>
+          <span className="bet-item__text">Guests wins</span>
+        </div>
+      </div>
+      <button
+        className={`button selected__bet-button ${select === 0 && 'disabled'}`}
+        disabled={select === 0}
+        onClick={() => navigate(MAIN_ROUTE)}
+        type="button"
+      >
+        Make a bet
+      </button>
     </div>
   )
 }
